2d8b298d424fec2a1eeeb44b9369f0dc5ccd9f78
Fajar Hidayatullah <69250754+Fajarsch@users.noreply.github.com>
2023-02-03 19:43:44 +0700

    Update Cube.cs

--- Mod The Cube/Assets/Scripts/Cube.cs
+++ Mod The Cube/Assets/Scripts/Cube.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    
+
+    private Color[] colors = { Color.red, Color.blue, Color.green, Color.yellow, Color.cyan };
+    private int colorIndex = 0;
+
+    private float delayTimeColor = 1;
+    private float repeatTimeColor = 0.2f;
+
+    public float delayScale = 1;
+    public float repeatScale = 1;
+
+    public float delayPos = 2;
+    public float repeatPos = 2;
+
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
-        
+        InvokeRepeating("ChangeColor", delayTimeColor, repeatTimeColor);
+        InvokeRepeating("ChangeScale", delayScale, repeatScale);
+        InvokeRepeating("ChangePosition", delayPos, repeatPos);
+
         Material material = Renderer.material;
         
         material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
@@ -18,6 +31,26 @@ public class Cube : MonoBehaviour
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(10.0f * Time.deltaTime, 10.0f * Time.deltaTime, 10.0f * Time.deltaTime);
+    }
+
+    void ChangeColor()
+    {
+        colorIndex = (colorIndex + 1) % colors.Length;
+        Renderer.material.color = colors[colorIndex];
+    }
+
+    void ChangeScale()
+    {
+        transform.localScale = Vector3.one * Random.Range(2.5f, 5.0f);
+    }
+
+    void ChangePosition()
+    {
+        float randomPosX = Random.Range(-20.0f, 20.0f);
+        float randomPosY = Random.Range(-5.0f, 5.0f);
+        float randomPosZ = Random.Range(-10.0f, 10.0f);
+
+        transform.position = new Vector3(randomPosX, randomPosY, randomPosZ);
     }
 }
